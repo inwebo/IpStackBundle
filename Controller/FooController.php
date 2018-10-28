78c4d63aedd37d57521a3eea9463bd409e2498bf
inwebo <gh_kvrg1hir>
2018-10-28 18:44:14 +0100

    Hello world

---
+++ Controller/FooController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inwebo
+ * Date: 28/10/18
+ * Time: 17:28
+ */
+
+namespace Inwebo\Bundle\FooBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class FooController extends Controller
+{
+    public function indexAction()
+    {
+        die('+');
+    }
+}
---
+++ InweboIpStackBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inwebo
+ * Date: 28/10/18
+ * Time: 17:16
+ */
+
+namespace Inwebo\Bundle\IpStackBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class InweboIpStackBundle extends Bundle
+{
+
+}
