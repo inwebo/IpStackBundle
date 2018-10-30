28cda0607e8b454e065be64754598210dc234785
inwebo <gh_kvrg1hir>
2018-10-30 23:19:04 +0100

    Step

--- Model/Client.php
+++ Model/Client.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: inwebo
- * Date: 30/10/18
- * Time: 20:54
- */
 
 namespace Inwebo\Bundle\IpStackBundle\Model;
 
--- Model/Factory.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Inwebo\Bundle\IpStackBundle\Model;
-
-class Factory extends FactoryAbstract
-{
-}
