84542b14893caaa808cbf055a49430b1dcff7b41
inwebo <gh_kvrg1hir>
2018-12-04 20:07:45 +0100

    Fixtures

---
+++ DependencyInjection/IpStackExtension.php
@@ -0,0 +1,23 @@
+<?php
+namespace Inwebo\Bundle\IpStackBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+
+class IpStackExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config/') );
+
+        try {
+            $loader->load('services.yaml');
+        } catch (\Exception $e) {
+
+        }
+
+    }
+}
