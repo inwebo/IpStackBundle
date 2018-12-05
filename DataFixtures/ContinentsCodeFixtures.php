73db6236024ae75bb41b3267209b15b724d02bca
julien <julien@francepronet.com>
2018-12-05 13:37:31 +0100

    Step

---
+++ DataFixtures/ContinentsCodeFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @author    Julien Hannotin <julien@ddf.agency>
+ *
+ * @copyright 2018 Digital Dealer Factory
+ *
+ * @license   All rights reserved
+ *
+ * Date: 05/12/18
+ * Time: 13:35
+ *
+ */
+
+namespace Inwebo\Bundle\IpStackBundle\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * Class ContinentsCodeFixtures
+ */
+class ContinentsCodeFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+    }
+}
